aa2ac6edfaaef3953117401b1299575da1413744
Elliott Arnold <elliottarnold@Elliotts-Air.attlocal.net>
2022-10-26 06:52:21 -0500

    stopping point

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Box, IconButton, useTheme } from '@mui/system'
 import styled from 'styled-components';
-import { RiNumber1, RiNumber2, RiNumber3, RiNumber4  } from "react-icons/ri";
+import { RiNumber1, RiNumber2, RiNumber3, RiNumber4 , RiMovieFill} from "react-icons/ri";
 
 import { AiFillPlayCircle } from "react-icons/ai";
 import { BsToggleOn,BsToggleOff } from "react-icons/bs";
@@ -19,24 +19,49 @@ export default function Sidebar() {
     </div>
 
     <div className='icons'>
-
-  
+          <div className="icons__header">
+          <RiMovieFill /> <span>SELECT</span>
+          </div>
+    
       <ul>
-      SELECT
+   
         <li onMouseOver={() => setHover(1)} onMouseLeave={() => {setHover(0)}}>
-         { hover == 1 ? <AiFillPlayCircle /> : <RiNumber1 /> }
+        <a>
+
+          <span> Video </span> { hover == 1 ? <AiFillPlayCircle /> : <RiNumber1 /> }
+
+
+        </a>
+
         </li>
 
         <li onMouseOver={() => setHover(2)} onMouseLeave={() => {setHover(0)}}>
-         { hover == 2 ? <AiFillPlayCircle /> : <RiNumber2 /> }
+
+        <a>
+
+            <span> Video </span> { hover == 2 ? <AiFillPlayCircle /> : <RiNumber2 /> }
+
+
+        </a>
+       
         </li>
 
         <li onMouseOver={() => setHover(3)} onMouseLeave={() => {setHover(0)}}>
-         { hover == 3 ? <AiFillPlayCircle /> : <RiNumber3 /> }
+        <a>
+
+          <span> Video </span> { hover == 3 ? <AiFillPlayCircle /> : <RiNumber3 /> }
+
+
+          </a>
         </li>
 
          <li onMouseOver={() => setHover(4)} onMouseLeave={() => {setHover(0)}}>
-         { hover == 4 ? <AiFillPlayCircle /> : <RiNumber4 /> }
+         <a>
+
+          <span> Video </span> { hover == 4 ? <AiFillPlayCircle /> : <RiNumber4/> }
+
+
+        </a>
         </li>
       </ul>
     </div>
@@ -55,54 +80,78 @@ width: 15vw;
 height: 100vh;
 position: fixed;
 
-${'' /* top: 0;
-z-index: -1; */}
 left: 0;
 transition: all .4s;
-${'' /* transform: translateX(-100%); */}
+
 
 transform: ${(props) => (props.open ? "translateX(-79%)": "translateX(0%)") };
 opacity: ${(props) => (props.open ? .4: 1) };
 transition: all 2.9s;
 background-color: white;
 display: flex;
-${'' /* justify-content: center; */}
+
 flex-direction: column;
 background-color: #003047 !important;
 .toggle {
   margin-left: 180px;
+  color: #06A2BF;
+  font-size: 1.9rem;
 }
 .icons {
 
+  &__header {
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   padding-top: 10%;
+   color: orange;
+   gap: 1rem;
+   font-size: 2rem;
+  }
   margin: auto;
   ul {
-    margin: 0 0 0 22px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 20%;
     width:100%;
+   
     padding: 20px;
     color: orange;
     ${'' /* background-color: orange; */}
-    border: 1px solid black;
+    ${'' /* border: 1px solid black; */}
     list-style-type: none;
-    line-spacing: 4rem;
+
+    span {
+      margin: auto;
+    }
+    
      li  {
-   
+      justify-content: center;
       transition: ease-in-out;
       align-items: center;
-      width: 60px;
+     
+     
+      width: 100%;
       height: 60px;
-      font-size: 2rem;
+      font-size: 1rem;
       display: flex;
       cursor: pointer; 
       color: orange; 
+      
+     }
+
+     li > a {
+      display: flex;
+      align-items: center;
+      letter-spacing: 12px;
      }
 
      li:nth-child(n) {
       justify-content: center;
       margin-top: 10px;
-      background-color: black;
-      text-align: center;
-      border-radius: 50%;
-
+      border-bottom: 1px solid  #06A2BF;
+  
       :hover {
         color: green;
         transition: .8s ease-in-out;
@@ -114,13 +163,5 @@ background-color: #003047 !important;
   }
 }
 
-.open {
-  transform: translateX(0%);
-  transition: all .4s;
-}
 
-.close {
-  transition: all .4s;
-  transform: translateX(-100%);
-}
 `
